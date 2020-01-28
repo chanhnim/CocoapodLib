29d221034fa791debe3263530edb609615c8df94
Chanh Nim <chanh.nim@volvocars.com>
2020-01-28 15:40:39 -0800

    Initial Commit

---
+++ CocoapodLib/CocoapodLib.h
@@ -0,0 +1,19 @@
+//
+//  CocoapodLib.h
+//  CocoapodLib
+//
+//  Created by Nim, Chanh on 1/28/20.
+//  Copyright © 2020 Nim, Chanh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoapodLib.
+FOUNDATION_EXPORT double CocoapodLibVersionNumber;
+
+//! Project version string for CocoapodLib.
+FOUNDATION_EXPORT const unsigned char CocoapodLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoapodLib/PublicHeader.h>
+
+
