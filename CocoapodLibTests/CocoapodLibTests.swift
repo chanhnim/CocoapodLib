60ce8e540eede1806908241ba19592a20ddebf08
Chanh Nim <chanh.nim@volvocars.com>
2020-01-28 16:37:40 -0800

    unit test

--- CocoapodLibTests/CocoapodLibTests.swift
+++ CocoapodLibTests/CocoapodLibTests.swift
@@ -37,5 +37,10 @@ class CocoapodLibTests: XCTestCase {
     func testAdd() {
         XCTAssertEqual(swiftyLib?.add(a: 1, b: 1), 2)
     }
+    
+    func testSub() {
+        XCTAssertEqual(swiftyLib?.sub(a: 2, b: 1), 1)
+    }
+
 
 }
