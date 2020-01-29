0ca3d106c7404bf540f062f486ff1ac7d8fe1c4e
Chanh Nim <chanh.nim@volvocars.com>
2020-01-28 16:29:46 -0800

    functions

---
+++ CocoapodLib/SwiftLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftLib.swift
+//  CocoapodLib
+//
+//  Created by Nim, Chanh on 1/28/20.
+//  Copyright © 2020 Nim, Chanh. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftLib {
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
